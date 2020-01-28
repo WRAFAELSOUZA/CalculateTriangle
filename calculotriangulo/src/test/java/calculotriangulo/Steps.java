8d02942a6dc83536c506d4d1cb70391e6ad73494
WRAFAELSOUZA <32987462+WRAFAELSOUZA@users.noreply.github.com>
2020-01-28 17:47:17 -0300

    update Steps class

--- calculotriangulo/src/test/java/calculotriangulo/Steps.java
+++ calculotriangulo/src/test/java/calculotriangulo/Steps.java
@@ -3,10 +3,8 @@ package calculotriangulo;
 import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -21,10 +19,8 @@ public class Steps {
 	@Before
 	public void startChrome() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\rafael.souza\\git\\calculateTriangle\\calculotriangulo\\drivers\\Chrome\\79\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions ();
-		options.addArguments("--headless");
-		driver = new ChromeDriver(options);
+				"C:\\WorkSpaces\\calculartriangulo\\webdriver\\chrome\\79\\chromedriver.exe");
+				driver = new ChromeDriver();
 	}
 
 	/*public static void startFirefox() {
@@ -35,36 +31,35 @@ public class Steps {
 
 	public static void startIE() {
 		System.setProperty("webdriver.ie.driver",
-				"C:\\Users\\rafael.souza\\eclipse-workspace\\calculotriangulo\\drivers\\Chrome\\79\\chromedriver.exe");
+				"C:\\WorkSpaces\\calculateTriangle\\calculateTriangle\\webdriver\\IE\\IEDriverServer.exe");
 		driver = new InternetExplorerDriver();
 	}
-	*/
+*/
 	@Given("^the user access the url \"([^\"]*)\"$")
-	public void the_user_access_the_url(String arg1) throws Throwable {
-		driver.get(arg1);
+	public void the_user_access_the_url(String url) throws Throwable {
+			driver.get(url);
 	}
 
 	@And("^the user write the value in (.*),(.*),(.*)$")
-	public void the_user_write_the_value_in(String arg1, String arg2, String arg3) throws Throwable {
-		driver.findElement(By.id("lado1")).sendKeys(arg1 + Keys.TAB);
-		driver.findElement(By.id("lado2")).sendKeys(arg2 + Keys.TAB);
-		driver.findElement(By.id("lado3")).sendKeys(arg3);
+	public void the_user_write_the_value_in(String side1, String side2, String side3) throws Throwable {
+		driver.findElement(By.id("lado1")).sendKeys(side1);
+		driver.findElement(By.id("lado2")).sendKeys(side2);
+		driver.findElement(By.id("lado3")).sendKeys(side3);
+		
 	}
 
 	@When("^the user press on \"([^\"]*)\" button$")
-	public void the_user_press_on_button(String arg1) throws Throwable {
+	public void the_user_press_on_button(String calcular) throws Throwable {
 		driver.findElement(By.id("calcular")).click();
 	}
 
 	@Then("^the result is \"([^\"]*)\"$")
-	public void the_result_is(String arg1) throws Throwable {
-		assertEquals(arg1, driver.findElement(By.id("message")).getText());
+	public void the_result_is(String result) throws Throwable {
+		assertEquals(result, driver.findElement(By.id("message")).getText());
 	}
 
 	@After
-	public static void end() {
+	public void end() {
 		driver.quit();
-
 	}
-
 }
